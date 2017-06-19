6a9e7f8342276181502a75999bca7b7d778f2c3e
Robert Graupner <robertgraupner@air-robert.home>
2017-06-19 23:52:13 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var a = -1;
+var b = 7;
+var value = (a * a) + (2 * a * b) - (b * b);
+console.log(value);
+if (value > 0) {
+    console.log('wynik dodatni');
+} 
+else if (value == 0) {
+	console.log('wynik równy 0');
+}
+else {
+    console.log('wynik ujemny');
+}
